bc02d839a95e25d27b0bdb693c1b9242e1fc0173
Gadis QS HQ <qsgadis449@gmail.com>
2026-02-23 04:59:00 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,45 +1,21 @@
-const CACHE_NAME = 'gadis-qshq-cache-v3';
-
+const CACHE_NAME = "gadis-hq-v1";
 const urlsToCache = [
-  './',
-  './index.html',
-  './semakkod.html',
-  './dashboard.html',
-  './audience-screen.html',
-  './css/style.css',
-  './js/config.js',
-  './js/semakkod.js',
-  './js/dashboard.js',
-  './js/audience.js',
-  './js/api.js',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  "/ganjaran/",
+  "/ganjaran/index.html",
+  "/ganjaran/dashboard.html",
+  "/ganjaran/manifest.json"
 ];
 
-self.addEventListener('install', event => {
-  self.skipWaiting();
+self.addEventListener("install", event=>{
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    .then(cache=>cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
-      )
-    )
-  );
-  return self.clients.claim();
-});
-
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event=>{
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    .then(response=>response || fetch(event.request))
   );
 });
