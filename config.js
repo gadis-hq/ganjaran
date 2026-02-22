6e154731694f242753e7cb15331a00a70a679277
Gadis QS HQ <qsgadis449@gmail.com>
2026-02-23 00:09:49 +0800

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-const CONFIG = {
-    API_URL: "https://script.google.com/macros/s/AKfycbxKXIu6STJewe_562xKMdqaeSN20fZpdLWFVqW_x2IvLQB13G6YZwHmfa845Egy8tC9/exec", // Ganti dengan URL Apps Script
-    CONFETTI_COLORS: ['#ff99cc','#d4af37','#fff0f5'],
-    BADGE_COLORS: {
-        "TELAH DITEBUS": "#d4af37",
-        "BELUM DITEBUS": "#28a745",
-        "TIDAK SAH": "#dc3545"
-    }
-};
