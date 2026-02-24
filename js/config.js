b710368a6d04631501a2090b6c2b4614e279d2ad
Gadis QS HQ <qsgadis449@gmail.com>
2026-02-25 03:20:14 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    API_URL: "https://script.google.com/macros/s/AKfycbxKXIu6STJewe_562xKMdqaeSN20fZpdLWFVqW_x2IvLQB13G6YZwHmfa845Egy8tC9/exec", // Ganti dengan URL Apps Script
+    API_URL: "https://script.google.com/macros/s/AKfycbw34iaobuZVWwmJ7vlLqOCKzTedVSgwqcYpyPDZiMYUUUqXTLNAFnX7m9TwzZI7RXYh/exec", // Ganti dengan URL Apps Script
     CONFETTI_COLORS: ['#ff99cc','#d4af37','#fff0f5'],
     BADGE_COLORS: {
         "TELAH DITEBUS": "#d4af37",
