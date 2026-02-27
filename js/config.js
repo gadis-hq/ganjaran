05865b3b476ccadbd7c9cb5167ceb5546b040950
Gadis QS HQ <qsgadis449@gmail.com>
2026-02-28 05:46:18 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,9 +1,9 @@
-const CONFIG = {
-    API_URL: "https://script.google.com/macros/s/AKfycby6m-F4D2RN7C8l2e7n_gZuvOrBeAkDhIlu6s5LCS_kDp3QWl9_iQw_1xdZ1srMVuQL/exec", // Ganti dengan URL Apps Script
-    CONFETTI_COLORS: ['#ff99cc','#d4af37','#fff0f5'],
-    BADGE_COLORS: {
+export const CONFIG = {
+  BASE_URL: "https://gadis-hq.github.io/ganjaran/",
+  WEB_APP: "WEB_APP_URL_PUAN",
+  SECRET_KEY: "GADIS_SECRET_KEY", 
+  BADGE_COLORS:
         "TELAH DITEBUS": "#d4af37",
         "BELUM DITEBUS": "#28a745",
-        "TIDAK SAH": "#dc3545"
-    }
+        "TIDAK SAH": "#dc3545"  
 };
