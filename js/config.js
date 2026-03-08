9b9264c630afb34e1e2074583c0d10e19103c8d8
Gadis QS HQ <qsgadis449@gmail.com>
2026-03-09 01:28:06 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
   BASE_URL: "https://gadis-hq.github.io/ganjaran/",
-  WEB_APP: "WEB_APP_URL_PUAN",
+  WEB_APP: "https://script.google.com/macros/s/AKfycby6m-F4D2RN7C8l2e7n_gZuvOrBeAkDhIlu6s5LCS_kDp3QWl9_iQw_1xdZ1srMVuQL/exec",
   SECRET_KEY: "GADIS_SECRET_KEY", 
   BADGE_COLORS:
         "TELAH DITEBUS": "#d4af37",
